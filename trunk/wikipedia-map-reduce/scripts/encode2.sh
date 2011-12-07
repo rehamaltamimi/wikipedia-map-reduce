b0ffc704b89e2bbba586ad39aca42926dfce5daa
shilad <shilad@b2a19fd8-a0a8-c1d7-bab7-774666daff13>
2011-12-07 20:07:59 +0000

    Switched to GNU parallel instead of xargs -P

--- trunk/wikipedia-map-reduce/scripts/encode2.sh
+++ trunk/wikipedia-map-reduce/scripts/encode2.sh
@@ -1,8 +1,12 @@
 # This script processes a dump file by file.
+# The parallel command can be downloaded at http://www.gnu.org/software/parallel/
 #
 
 dest=$1
-num_procs=15
 shift
 
-echo $@ | tr ' ' ' \n' | xargs -n 1 -P $num_procs ./scripts/reencode_one.sh $dest
+mkdir -p $dest/logs
+
+echo $@ | \
+tr ' ' '\n' | \
+parallel ./scripts/reencode_one.sh $dest {}
--- trunk/wikipedia-map-reduce/scripts/reencode_one.sh
+++ trunk/wikipedia-map-reduce/scripts/reencode_one.sh
@@ -1,7 +1,6 @@
 dest_dir=$1
+log_dir=$1/logs
 src=$2
 file=`basename $src`
 res=$dest_dir/$file.hadoop
-log_py=$dest_dir/$file.hadoop.log
-log_7z=$dest_dir/$file.7z.log
-7za e -so $src 2>$log_7z | python2.6 ./scripts/reencode.py $res 2>$log_py
+7za e -so $src 2>$log_dir/$file.7z.log |  python2.6 ./scripts/reencode.py $res 2>$log_dir/$file.encode.log
