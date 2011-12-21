ea12a3d11383f6052a3d0026ab53b3f0956e8a8e
shilad <shilad@b2a19fd8-a0a8-c1d7-bab7-774666daff13>
2011-12-21 22:29:04 +0000

    Moved citation stuff into its own package.

--- trunk/wikipedia-map-reduce/src/wikiParser/citations/CitationCounter.java
+++ trunk/wikipedia-map-reduce/src/wikiParser/citations/CitationCounter.java
@@ -2,7 +2,7 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-package wikiParser.mapReduce.statistics;
+package wikiParser.citations;
 
 import java.io.IOException;
 import java.util.*;
