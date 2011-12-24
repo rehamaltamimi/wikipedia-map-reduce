61ff886ac3ae6b88f6bf90051fe9053d1b17b214
shilad <shilad@b2a19fd8-a0a8-c1d7-bab7-774666daff13>
2011-12-24 19:45:18 +0000

    Added current revision only optional arg.

--- wikipedia-map-reduce/scripts/reencode_one.sh
+++
@@ -1,6 +0,0 @@
-dest_dir=$1
-log_dir=$1/logs
-src=$2
-file=`basename $src`
-res=$dest_dir/$file.hadoop
-7za e -so $src 2>$log_dir/$file.7z.log |  python2.6 ./scripts/reencode.py $res 2>$log_dir/$file.encode.log
