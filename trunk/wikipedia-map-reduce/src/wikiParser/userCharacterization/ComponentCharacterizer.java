ea0901e02371356b9a3818f302b208bbb1964dfa
nathaniel.miller.20@gmail.com <nathaniel.miller.20@gmail.com@b2a19fd8-a0a8-c1d7-bab7-774666daff13>
2011-12-24 19:59:19 +0000

    User characterization using one part MapReduce, one part ThreadPoolExecutor.

---
+++ trunk/wikipedia-map-reduce/src/wikiParser/userCharacterization/ComponentCharacterizer.java
@@ -0,0 +1,221 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package wikiParser.userCharacterization;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.RejectedExecutionException;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Nathaniel
+ */
+public class ComponentCharacterizer {
+     private static ThreadPoolExecutor tpe;
+     private static LinkedBlockingQueue<String> results;
+     private static HashMap<Integer,String> clusterComponents;
+     private static HashMap<Integer,String> clusterChanges;
+     private static final int PRINT_INTERVAL = 10000;
+     private static final int QUEUE_LENGTH = 100000;
+     
+     public static void main(String args[]) throws FileNotFoundException, IOException {
+         if (args.length < 4) {
+             System.err.println("Usage: output clusterComponents coreSize clusterBytesAdded");
+         }
+         int coreSize = Integer.parseInt(args[2]);
+         tpe = new ThreadPoolExecutor(coreSize,coreSize,1,TimeUnit.SECONDS, new ArrayBlockingQueue(QUEUE_LENGTH));
+         results = new LinkedBlockingQueue<String>();
+         BufferedReader componentReader = new BufferedReader(new InputStreamReader(new FileInputStream(new File(args[1]))));
+         BufferedReader bytesChanged = new BufferedReader(new InputStreamReader(new FileInputStream(new File(args[3]))));
+         String components = componentReader.readLine();
+         String changes = bytesChanged.readLine();
+         Writer writer = null;
+         int submitted = 0;
+         while (components != null || changes != null) {
+             if (submitted == QUEUE_LENGTH/2) {
+                 writer = new Writer(results, args[0]);
+                 tpe.execute(writer);
+             }
+             if (components != null) {
+                 String[] split = components.split("\t");
+                 int componentsCluster = Integer.parseInt(split[0]);
+                 if (clusterChanges.containsKey(componentsCluster)) {
+                     //both lines have been read, queue task
+                     waitUntilQueued(new Characterizer(componentsCluster,split[1],clusterChanges.get(componentsCluster),results));
+                     submitted++;
+                 } else {
+                     if (split.length > 1) {
+                         //only one line has been read in and it has information
+                         //store data for later
+                         clusterComponents.put(componentsCluster, split[1]);
+                     } //else no data, do nothing
+                 }
+             }
+             if (changes != null) {
+                 String[] split = changes.split("\t");
+                 int changesCluster = Integer.parseInt(split[0]);
+                 if (clusterComponents.containsKey(changesCluster)) {
+                     //both lines from cluster have been read in, queue task
+                     waitUntilQueued(new Characterizer(changesCluster,split[1],clusterComponents.get(changesCluster),results));
+                     submitted++;
+                 } else {
+                     //only one line has been read, store data for later
+                     clusterChanges.put(changesCluster, split[1]);
+                 }
+             }
+             components = componentReader.readLine();
+             changes = bytesChanged.readLine();
+         }
+         writer.setTotalCases(submitted);
+     }
+     
+     /**
+      * Continues attempting to queue until tryExecute returns true
+      * (RejectedExecutionExceptions are only thrown when
+      * there is no room left in the queue, so it makes sense to
+      * wait for space to open up)
+      * @param c 
+      */
+     private static void waitUntilQueued(Characterizer c) {
+         boolean added = false;
+         while (!added) {
+             added = tryExecute(c);
+         } 
+     }
+     
+     /**
+      * 
+      * @param c
+      * @return true if successful, false if RejectedExecutionException thrown
+      */
+     private static boolean tryExecute(Characterizer c) {
+         try {
+             tpe.execute(c);
+         } catch (RejectedExecutionException e) {
+             return false;
+         }
+         return true;
+     }
+     
+     private static class Writer implements Runnable {
+
+         private int cases;
+         private boolean moreTasks;
+         private LinkedBlockingQueue<String> toWrite;
+         private String outputPath;
+         
+         public Writer(LinkedBlockingQueue<String> results, String outputPath) {
+             moreTasks = true;
+             toWrite = results;
+             this.outputPath = outputPath;
+         }
+         
+        public void setTotalCases (int cases) {
+            this.cases = cases;
+            moreTasks = false;
+        }
+
+        @Override
+        public void run() {
+            int written = 0;
+            try {
+                BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(new File(outputPath))));
+                while (moreTasks || written < cases) {
+                    String output = toWrite.take();
+                    writer.write(output);
+                    writer.newLine();
+                    written++;
+                }
+                writer.flush();
+            } catch (Exception ex) {
+                Logger.getLogger(ComponentCharacterizer.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+     }
+     
+     private static class Characterizer implements Runnable {
+
+         private String components;
+         private String changes;
+         private int cluster;
+         private LinkedBlockingQueue<String> results;
+         
+         public Characterizer(int cluster, String components, String changes, LinkedBlockingQueue<String> results) {
+             this.components = components;
+             this.changes = changes;
+             this.results = results;
+             this.cluster = cluster;
+         }
+         
+        @Override
+        public void run() {
+            /*
+             * Components 0 and 1 are from the first conflict
+             * 0 is first, 1 is second.
+             * 2 and 3 are from the second conflict, 2 first,
+             * 3 second.  And so on with higher numbers
+             */
+            //initialize data structures with input
+            HashMap<Integer,HashSet<Integer>> componentMapping = new HashMap<Integer,HashSet<Integer>>();
+            HashMap<Integer,Integer> userChanges = new HashMap<Integer,Integer>();
+            int side = 0;
+            for (String conflict : components.split("\\|")) {
+                componentMapping.put(side, new HashSet<Integer>());
+                componentMapping.put(side + 1, new HashSet<Integer>());
+                for (String component : conflict.split(" ")) {
+                    for (String user : component.split(",")) {
+                        int u = Integer.parseInt(user);
+                        componentMapping.get(side).add(u);
+                        userChanges.put(u,0);
+                    }
+                    side++;
+                }
+            }
+            for (String userInfo : changes.split("\\|")) {
+                String[] userDelta = userInfo.split("#");
+                int user = Integer.parseInt(userDelta[0]);
+                if (userChanges.containsKey(user)) {
+                    userChanges.put(user,Integer.parseInt(userDelta[1]));
+                }
+            }
+            StringBuilder sb = new StringBuilder();
+            //for each side, compute average without decimal
+            //add to output string
+            for (int component = 0; component < componentMapping.keySet().size()/2; component++) {
+                int totalZero = 0;
+                for (int user : componentMapping.get(component*2)) {
+                    totalZero += userChanges.get(user);
+                }
+                int averageZero = totalZero/componentMapping.get(component*2).size();
+                int totalOne = 0;
+                for (int user : componentMapping.get(component*2 + 1)) {
+                    totalOne += userChanges.get(user);
+                }
+                int averageOne = totalOne/componentMapping.get(component*2 + 1).size();
+                sb.append(averageZero).append(" ").append(averageOne).append("|");
+            }
+            boolean added = false;
+            while (!added) {
+                added = results.add(cluster + "\t" + sb.toString());
+            }
+        }
+         
+     }
+}
