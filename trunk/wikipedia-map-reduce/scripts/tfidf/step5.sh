7d56f40436b40b8c9aadead85d02397170570c15
shilad <shilad@b2a19fd8-a0a8-c1d7-bab7-774666daff13>
2012-01-07 04:43:09 +0000

    Added scripts to kick off hadoop jobs.

---
+++ trunk/wikipedia-map-reduce/scripts/tfidf/step5.sh
@@ -0,0 +1,19 @@
+jar_dest=jobjars/$RANDOM/wikipediamapreduce.jar
+echo "placing jar in $jar_dest"
+
+s3cmd put ./dist/wikipedia-map-reduce.jar s3://macademia/$jar_dest || exit 1
+
+elastic-mapreduce --create --name 'Step5FinalDocSim' \
+    --instance-group master --instance-type cc2.8xlarge \
+    --instance-count 1 \
+    --instance-group core --instance-type cc2.8xlarge \
+    --instance-count 5 \
+    --instance-group task --instance-type cc2.8xlarge \
+    --instance-count 14 --bid-price 1.00  \
+    --bootstrap-action s3://elasticmapreduce/bootstrap-actions/configure-hadoop \
+    --args "-s,mapred.map.tasks=750,-s,mapred.reduce.tasks=750,-s,mapred.tasktracker.map.tasks.maximum=14,-s,mapred.tasktracker.reduce.tasks.maximum=14,-s,mapred.task.timeout=2000000" \
+    --log-uri s3://macademia/nbrvz/logs \
+    --enable-debugging \
+    --jar s3n://macademia/$jar_dest \
+    --main-class wmr.tfidf.Step5FinalDocSim \
+    --args "s3n://macademia/nbrvz/tf-idf-words/step4/,s3n://macademia/nbrvz/tf-idf-words/step5/"
