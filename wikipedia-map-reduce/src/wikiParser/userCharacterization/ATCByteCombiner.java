ff82ddc244fbec0c15f1af0157de91b819b5ddd6
nathaniel.miller.20 <nathaniel.miller.20@b2a19fd8-a0a8-c1d7-bab7-774666daff13>
2012-02-03 04:16:28 +0000

    Change ATCByteCombiner to reflect presence of ArticleTitlesToIds.

--- wikipedia-map-reduce/src/wikiParser/userCharacterization/ATCByteCombiner.java
+++ wikipedia-map-reduce/src/wikiParser/userCharacterization/ATCByteCombiner.java
@@ -14,7 +14,6 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.util.HashMap;
-import java.util.HashSet;
 
 /**
  *
@@ -22,80 +21,31 @@ import java.util.HashSet;
  * Takes output from DeltaByteCounter and makes it ready for ComponentCharacterizer
  * Input:
  *  Stream output from DeltaByteCounter
- *  Command line arguments should be an article titles to article IDs mapping
- *      and an article clusters file
+ *  Command line arguments should be an article clusters file with Wikipedia article IDs
+ *      and an output directory
  */
 public class ATCByteCombiner {
     
     public static void main(String args[]) throws FileNotFoundException, IOException {
         if (args.length < 4) {
-            System.out.println("Usage: [articleIDs articleList clusters outputDirectory]");
+            System.out.println("Usage: [clusters outputDirectory]");
             System.exit(0);
         }
         
-        HashMap<String, Long> allArticles = new HashMap<String, Long>();
+        HashMap<Long,Long> aidCluster = new HashMap<Long,Long>(); //Maps from article ID to cluster #
         
         BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(new File(args[0]))));
         String line = reader.readLine();
-        System.out.println("Reading article to IDs file...");
-        int lines = 0;
-        while (line != null) {
-            if (lines % 1000000 == 0) {
-                System.out.println("Read " + lines + " articles...");
-            }
-            String[] info = line.split("\t");
-            if (info.length > 1) {
-                allArticles.put(info[0].intern(), Long.parseLong(info[1]));
-            }
-            line = reader.readLine();
-            lines++;
-        }
-        
-        HashMap<String,Long> articles = new HashMap<String,Long>();
-        reader = new BufferedReader(new InputStreamReader(new FileInputStream(new File(args[1]))));
-        System.out.println("Reading article list...");
-        line = reader.readLine();
-        int articlesRead = 0;
-        while (line != null) {
-            String title = line.split("\t")[0];
-            if (allArticles.containsKey(title)) {
-                articles.put(title, allArticles.get(title));
-                articlesRead++;
-            } else {
-                System.out.println("Title not found: " + title);
-            }
-            line = reader.readLine();
-        }        
-        
-        HashMap<Long,Long> aidCluster = new HashMap<Long,Long>(); //Maps from article ID to cluster #
-        //Articles not in clusters are represented by the negative of their article ID
-        
-        reader = new BufferedReader(new InputStreamReader(new FileInputStream(new File(args[2]))));
-        line = reader.readLine();
         System.out.println("Reading article clusters file...");
         long cluster = 0;
         while (line != null) {
-            for (String title : line.split("\\|")) {
-                Long i = articles.get(title);
-                articles.remove(title);
-                aidCluster.put(i,cluster);
+            for (String id : line.split("\\|")) {
+                aidCluster.put(Long.parseLong(id),cluster);
             }
             cluster++;
             line = reader.readLine();
         }
-        System.out.println("Read in " + cluster + " clusters with " + articlesRead + " articles.");
-        for(String title : articles.keySet()) { //Only articles not in clusters
-            if (articles.containsKey(title)) {
-                try {
-                    aidCluster.put(articles.get(title),-articles.get(title));
-                } catch (NullPointerException e) {
-                    System.out.println("NPE:");
-                    System.out.println("AID: " + aidCluster + ", articles: " + articles + ", title: " + title);
-                }
-            } else {
-                System.out.println("Bad article title conversion: " + title);
-            }
-        }
+        System.out.println("Read in " + cluster + " clusters.");
         
         HashMap<Long,HashMap<String,Integer>> clusterDBytes = new HashMap<Long,HashMap<String,Integer>>();
         reader = new BufferedReader(new InputStreamReader(System.in));
@@ -128,7 +78,7 @@ public class ATCByteCombiner {
         }
         
         System.out.println("Writing output...");
-        BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(new File(args[3]))));
+        BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(new File(args[1]))));
         for (long c : clusterDBytes.keySet()) {
             StringBuilder sb = new StringBuilder();
             sb.append(c).append("\t");
