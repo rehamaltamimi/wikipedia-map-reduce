3149fcdc7d3bf903efbeb283401dd4e0a74c5821
shilad <shilad@b2a19fd8-a0a8-c1d7-bab7-774666daff13>
2012-05-05 03:53:34 +0000

    Cleaned up revert graph generator. Ready to go!

---
+++ trunk/wikipedia-map-reduce/src/wmr/reverts/RevertGraphGenerator.java
@@ -0,0 +1,130 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package wmr.reverts;
+
+
+import java.io.IOException;
+import java.util.*;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.conf.Configured;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.*;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
+import org.apache.hadoop.util.Tool;
+import org.apache.hadoop.util.ToolRunner;
+import wmr.core.*;
+
+/**
+ *
+ * @author Shilad Sen
+ * 
+ */
+public class RevertGraphGenerator extends Configured implements Tool {
+    static final int WINDOW_SIZE = 5;
+
+    /*
+     * Takes key-value 7zip hashes and outputs url-id pairs.
+     */
+    public static class MyMapper extends Mapper<Long, AllRevisions, Text, Text> {
+
+        @Override
+        public void map(Long pageId, AllRevisions revs, Mapper.Context context) throws IOException, InterruptedException {
+            Page article = revs.getPage();
+            User lastEditor = null;
+            context.progress();
+            if (article.isNormalPage()) {
+                Queue<Long> fingerprints = new LinkedList<Long>();
+                for (Revision rev : revs.getRevisions()) {
+                    context.progress();
+                    if (rev == null) {
+                        continue;
+                    }
+
+                    Long fingerprint = rev.getTextFingerprint();
+                    String code = null;
+                    if (rev.isVandalismStrictRevert()) {
+                        code = "vs";
+                    } else if (rev.isVandalismLooseRevert()) {
+                        code = "vl";
+                    } else if (fingerprints.contains(fingerprint)) {
+                        code = "f";
+                    }
+                    if (code != null && rev.getContributor().isBot()) {
+                        code = "b" + code;
+                    }
+                    if (code != null && rev.getText().length() < 20) {
+                        code += "s" + code;
+                    }
+                    if (code != null && lastEditor != null) {
+                        context.write(
+                                new Text(pageId + "@" + article.getName()),
+                                new Text(
+                                    rev.getId() + "\t" +
+                                    rev.getTimestamp() + "\t" +
+                                    code + "\t" +
+                                    lastEditor.getId() + "@" + lastEditor.getName() + "\t" +
+                                    rev.getContributor().getId() + "@" + rev.getContributor().getName())
+                            );
+                    }
+                    fingerprints.offer(fingerprint);
+                    if (fingerprints.size() > WINDOW_SIZE) {
+                        fingerprints.remove();
+                    }
+
+                    lastEditor = rev.getContributor();
+                }
+            }
+        }
+    }
+
+    public int run(String args[]) throws Exception {
+
+        if (args.length < 2) {
+            System.out.println("usage: [input output]");
+            ToolRunner.printGenericCommandUsage(System.out);
+            return -1;
+        }
+
+        Path inputPath = new Path(args[0]);
+        Path outputPath = new Path(args[1]);
+
+        Configuration conf = getConf();
+        Job job = new Job(conf, this.getClass().toString());
+
+        FileInputFormat.setInputPaths(job, inputPath);
+        FileOutputFormat.setOutputPath(job, outputPath);
+
+        
+        job.setJarByClass(RevertGraphGenerator.class);
+        job.setInputFormatClass(AllRevisionsInputFormat.class);
+        job.setOutputFormatClass(TextOutputFormat.class);
+        job.setMapOutputKeyClass(Text.class);
+        job.setMapOutputValueClass(Text.class);
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(Text.class);
+        
+        job.setMapperClass(MyMapper.class);
+        job.setReducerClass(Reducer.class); // identity reducer
+        FileSystem hdfs = FileSystem.get(outputPath.toUri(), conf);
+        if (hdfs.exists(outputPath)) {
+            hdfs.delete(outputPath, true);
+        }
+
+        return job.waitForCompletion(true) ? 0 : 1;
+    }
+
+    /**
+     * Dispatches command-line arguments to the tool via the
+     * <code>ToolRunner</code>.
+     */
+    public static void main(String[] args) throws Exception {
+        int res = ToolRunner.run(new RevertGraphGenerator(), args);
+        System.exit(res);
+    }
+}
