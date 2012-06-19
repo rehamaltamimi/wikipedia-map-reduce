8256c37af53a87de71463774c2ed66c2762340ec
b2utor21@gmail.com <b2utor21@gmail.com@b2a19fd8-a0a8-c1d7-bab7-774666daff13>
2012-06-19 22:07:31 +0000

    Add the "EditorTimes.sh" script, and the "EditorTimes.java".

---
+++ trunk/wikipedia-map-reduce/scripts/health/EditorTimes.sh
@@ -0,0 +1,27 @@
+bucket=s3://macalester                          # s3 bucket
+health=$bucket/health                           # everything for maxima goes under here
+input=$bucket/wikipedia/en-2011-11-15/          # the processed full text input file
+output=$health/out/editortimes/                 # output directory. CAREFUL - WILL BE OVERWRITTEN!
+logs=$health/logs
+class=wmr.health.EditorTimes
+
+jar_dest=$health/jobjars/$RANDOM/wikipediamapreduce.jar
+echo "placing jar in $jar_dest"
+
+
+s3cmd put ./dist/wikipedia-map-reduce.jar $jar_dest || exit 1
+
+elastic-mapreduce --create --name 'EditorTimes' \
+    --instance-group master --instance-type cc1.4xlarge \
+    --instance-count 1 \
+    --instance-group core --instance-type cc1.4xlarge \
+    --instance-count 1 \
+    --instance-group task --instance-type cc2.8xlarge \
+    --instance-count 6 --bid-price 2.00  \
+    --bootstrap-action s3://elasticmapreduce/bootstrap-actions/configure-hadoop \
+    --args "-s,mapred.task.timeout=2000000" \
+    --log-uri $logs \
+    --enable-debugging \
+    --jar $jar_dest \
+    --main-class $class \
+    --args "$input,$output"
